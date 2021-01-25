38fadb6faf30c103531fa5fcd52b3f110e7582d0
John Chen <john@semantichealth.ai>
2021-01-25 15:26:11 -0500

    use processed mimic data

--- examples/language-modeling/run_mlm_command.sh
+++ examples/language-modeling/run_mlm_command.sh
@@ -1,5 +1,5 @@
 
 
 
-python run_mlm.py --model_name_or_path emilyalsentzer/Bio_ClinicalBERT --output_dir out_mimic_clinicalbert --overwrite_output_dir --line_by_line --train_file sample_data.txt --validation_file sample_data_new.txt --do_train --max_seq_length 128
+python run_mlm.py --model_name_or_path emilyalsentzer/Bio_ClinicalBERT --output_dir EXP_out_mimic_clinicalbert --overwrite_output_dir --line_by_line --train_file /sh/sh-experiments/inputs/multinote_balanced/generated/pretrain_train.txt --validation_file /sh/sh-experiments/inputs/multinote_balanced/generated/pretrain_valid.txt --do_train --max_seq_length 128
 
